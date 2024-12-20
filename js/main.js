fdbfaf3444ebee423caae42e06137b161664e526
minhdang0 <dangnhatminh8402@gmail.com>
2024-12-21 03:32:46 +0700

    push

--- js/main.js
+++ js/main.js
@@ -2,22 +2,24 @@ document.getElementById("menu-toggle").addEventListener("click", function() {
     const navLinks = document.getElementById("nav-links");
     navLinks.classList.toggle("active");
   });
-// document.addEventListener("DOMContentLoaded", function () {
-//     const prevButton = document.querySelector('.pre');
-//     const nextButton = document.querySelector('.next');
-//     const items = document.querySelectorAll('.trending-card');
-//     let index = 0;
-//     function updateItems() {
-//         items.forEach(item => item.style.display = 'none');
-//         items[index].style.display = 'block';
-//     }
-//     nextButton.addEventListener('click', () => {
-//         index = (index + 1) % items.length;
-//         updateItems();
-//     });
-//     prevButton.addEventListener('click', () => {
-//         index = (index - 1 + items.length) % items.length;
-//         updateItems();
-//     });
-//     updateItems();
-// });
+
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () =>{
+  if (window.scrollY > 590) {
+    header.classList.add('scrolled'); 
+  } else {
+    header.classList.remove('scrolled');
+  }
+})
+
+
+function setProgress(circle, percentage) {
+  const radius = circle.r.baseVal.value;
+  const circumference = 2 * Math.PI * radius;
+  circle.style.strokeDasharray = `${circumference}`;
+  circle.style.strokeDashoffset = `${circumference - (percentage / 100) * circumference}`;
+}
+
+
+setProgress(document.querySelector('.stat-1 .progress'), 39);
